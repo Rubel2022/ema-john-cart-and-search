03cfe3ef7f25b3d95fe860cc7e0bdba2d7a220e5
Rubel Talukder <talukderrubel223@gmail.com>
2022-03-24 08:56:03 +0600

    full site added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+  const {cart}=props;
+  let total=0;
+  let shipping = 0;
+  let quantity =0;
+  for(const product of cart){
+    quantity=quantity + product.quantity;
+    total = total + product.price * product.quantity;
+    shipping = shipping + product.shipping * product.quantity;
+   
+  }
+  const tax =(total*10 / 100).toFixed(2)
+  const grandTotal=total + shipping + parseFloat(tax)
   return (
-    <div>
+    <div className='cart_style'>
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: $ {shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Grand Total: ${parseFloat(grandTotal.toFixed(2))}</h4>
     </div>
   );
 };
